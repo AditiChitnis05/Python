8b0b175f3310627b6dff06cd45e355733065de30
Aditi Chitnis <aditichitnis05@gmail.com>
2026-05-15 21:07:26 +0530

    practice ques

--- Python Collection/Inheritance.py
+++ Python Collection/Inheritance.py
@@ -1,3 +1,4 @@
+'''
 class Employee:
     def __init__(self, name, salary):
         self.name=name
@@ -29,3 +30,75 @@ d1.code()
 d2= Manager("Aditya", 70000000, 100)
 d2.display()
 d2.manage()
+
+
+class Vehicle:
+    def hello(self):
+        print("This is a vehicle")
+    def stop(self):
+        print("vehicle stopped")
+
+class Car(Vehicle):
+    def cars(self):
+        print("Hi i am a car")
+
+c=Car()
+
+c.hello()
+c.stop()
+c.cars()
+
+
+
+
+class Grandfather():
+    def land(self):
+        print("Grandpa owns land")
+    
+class Father(Grandfather):
+    def house(self):
+        print("Father built house on grandpas land")
+
+    def car(self):
+        print("I own car")
+
+class Son(Father):
+    def Bike(self):
+        print("Son inherited land and house he sold it to get a bike")
+
+c=Son()
+
+c.land()
+c.house()
+c.Bike()
+c.car()
+
+'''
+
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang")       #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747")     #Create a Plane object
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
